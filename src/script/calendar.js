6f9ce4bf2d018879e4a89455f42e7fd3def733cb
unknown <Taraspetrenko04@gmail.com>
2020-03-21 20:19:07 +0200

    add calendar

---
+++ src/script/calendar.js
@@ -0,0 +1,13 @@
+'use strict';
+const icon = document.querySelector('.calendar');
+const pannel = document.querySelector('.pannel__modal');
+
+
+icon.addEventListener('click', () => {
+    toggleModal();
+})
+
+
+function toggleModal(){
+    pannel.classList.toggle('active-mod');
+}
