b3daad3ab7ecc5184a484de9587ccc3ece8e6b39
unknown <Taraspetrenko04@gmail.com>
2020-03-29 14:54:38 +0300

    make post request

--- src/script/script.js
+++ src/script/script.js
@@ -1,15 +1,17 @@
 'use strict';
 const header = ['Пользователь', 'Дата регистрации', 'Последняя активность', 'Последнее действие']
 let users = [
-    { userEmail: "A@ukr.net", registrationDate: "Март 20,2020", lastActivity: "Март 20,2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
-    { userEmail: "B@ukr.net", registrationDate: "Март 20,2020", lastActivity: "Март 20,2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
-    { userEmail: "C@ukr.net", registrationDate: "Март 20,2020", lastActivity: "Март 20,2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
-    { userEmail: "D@ukr.net", registrationDate: "Март 20,2020", lastActivity: "Март 20,2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
-    { userEmail: "E@ukr.net", registrationDate: "Март 20,2020", lastActivity: "Март 20,2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
-    { userEmail: "F@ukr.net", registrationDate: "Март 20,2020", lastActivity: "Март 20,2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
-    { userEmail: "G@ukr.net", registrationDate: "Март 20,2020", lastActivity: "Март 20,2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
-    { userEmail: "H@ukr.net", registrationDate: "Март 20,2020", lastActivity: "Март 20,2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
-    { userEmail: "I@ukr.net", registrationDate: "Март 20,2020", lastActivity: "Март 20,2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "A@ukr.net", registrationDate: "Март 29, 2020", lastActivity: "Март 29, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "A@ukr.net", registrationDate: "Март 28, 2020", lastActivity: "Март 28, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "A@ukr.net", registrationDate: "Март 28, 2020", lastActivity: "Март 28, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "B@ukr.net", registrationDate: "Март 27, 2020", lastActivity: "Март 27, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "C@ukr.net", registrationDate: "Март 27, 2020", lastActivity: "Март 27, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "D@ukr.net", registrationDate: "Март 20, 2020", lastActivity: "Март 20, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "E@ukr.net", registrationDate: "Март 15, 2020", lastActivity: "Март 15, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "F@ukr.net", registrationDate: "Март 10, 2020", lastActivity: "Март 10, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "G@ukr.net", registrationDate: "Март 02, 2020", lastActivity: "Март 02, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "H@ukr.net", registrationDate: "Февраль 06, 2020", lastActivity: "Март 20, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
+    { userEmail: "I@ukr.net", registrationDate: "Февраль 20, 2020", lastActivity: "Март 20, 2020", lastAction: "How to fix relationship How to fix relationship How to fix relationship" },
 ]
 
 let btnUsers = document.querySelector('.users');
@@ -20,9 +22,29 @@ btnUsers.addEventListener('click', () => {
 });
 
 
-let tableBody = document.querySelector('.contacts__table');
+//Hide show modal Window
+const icon = document.querySelector('.calendar');
+const pannel = document.querySelector('.pannel__modal');
+const cancel = document.querySelector('.modal__cancel');
+const refresh = document.querySelector('.modal__refresh');
+icon.addEventListener('click', () => {
+    toggleModal();
+})
+cancel.addEventListener('click', () => {
+    toggleModal();
+})
+refresh.addEventListener('click', () => {
+    toggleModal();
+})
+function toggleModal(){
+    pannel.classList.toggle('active-mod');
+}
+
+
+
 
 //Table
+let tableBody = document.querySelector('.contacts__table');
 async function getUsers(arr, header) {
     try {
         text.classList.add('hide');
@@ -76,53 +98,197 @@ async function getUsers(arr, header) {
 
 
 // Calendar
-$(function() {
-  $('#date_range').datepicker({
-    range: 'period', // возможные значения: period, multiple
-    range_multiple_max: 2, // максимальное число выбранных дат в режиме "Несколько дат"
-    onSelect: function(dateText, inst, extensionRange) {
-    	// extensionRange - объект расширения
-      $('[name=startDate]').val(extensionRange.startDateText);
-      $('[name=endDate]').val(extensionRange.endDateText);
-    }
-  });
+$(function () {
+    $('#date_range').datepicker({
+        range: 'period', // возможные значения: period, multiple
+        range_multiple_max: 2, // максимальное число выбранных дат в режиме "Несколько дат"
+        onSelect: function (dateText, inst, extensionRange) {
+            // extensionRange - объект расширения
+            $('[name=startDate]').val(extensionRange.startDateText);
+            $('[name=endDate]').val(extensionRange.endDateText);
+        }
+    });
 
-  $('#date_range').datepicker('setDate', ['d', '']);
-  var extensionRange = $('#date_range').datepicker('widget').data('datepickerExtensionRange');
-  if(extensionRange.startDateText) $('[name=startDate]').val(extensionRange.startDateText);
-  if(extensionRange.startDateText) $('[name=startDate]').val(extensionRange.startDateText);
-  if(extensionRange.endDateText) $('[name=endDate]').val(extensionRange.endDateText);
+    $('#date_range').datepicker('setDate', ['d', '']);
+    var extensionRange = $('#date_range').datepicker('widget').data('datepickerExtensionRange');
+    if (extensionRange.startDateText) $('[name=startDate]').val(extensionRange.startDateText);
+    if (extensionRange.startDateText) $('[name=startDate]').val(extensionRange.startDateText);
+    if (extensionRange.endDateText) $('[name=endDate]').val(extensionRange.endDateText);
 });
 
 
 //Modal Window
+let timeRange = '';
+
 document.getElementsByClassName('modal__list')[0].addEventListener('click', (event) => {
     try {
-
-
-        function swithActiveButton(event){
+        function swithActiveButton(event) {
             [...document.getElementsByClassName('modal__item')].forEach(elem => { elem.classList.remove('modal__active') })
             event.target.classList.add('modal__active');//add current active
-         };
-         swithActiveButton(event);
-         
-         
-         let date = new Date();
-         let month = new Date().toLocaleString('ru', { month: 'long' });
-         console.log(month[0].toUpperCase()+month.slice(1) + ' ' + date.getDay() + ',' + date.getFullYear() )
-         
-        
+            document.querySelector('.modal__list').classList.remove('modal__active');
+        };
+        swithActiveButton(event);
+
+        timeRange = event.target.dataset.range; //return data range
+        return
     }
     catch (err) {
         alert('Ошибка ' + err.name);
     }
+})
 
-     
- })
+// массив и промежуток времени
+document.getElementsByClassName('modal__refresh')[0].addEventListener('click', () => {
+    try {
+        let newUsers = [];
+        let date = new Date();
+        let rangeFrom = '';
+        let rangeTo = '';
+
+        if (timeRange === '') {
+            getUsers(users, header);
+            showRange('Весь срок');
 
 
- function getRange( lastDay ){
+        } else if (timeRange === '1') {
+            newUsers = users.filter(user => {
+                let currDate = (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate());
+                rangeFrom = 'Сегодня';
+                return currDate === convertFormatData(user.registrationDate)
+            });
+            getUsers(newUsers, header);
+            showRange(rangeFrom);
 
 
-    // return new Table sort
- }
+        } else if (timeRange === '-1') {
+            newUsers = users.filter(user => {
+                let yestDate = (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + (date.getDate() - 1));
+                rangeFrom = 'Вчера';
+                return yestDate === convertFormatData(user.registrationDate)
+            });
+            getUsers(newUsers, header);
+            showRange(rangeFrom);
+
+
+        } else {
+            newUsers = users.filter(user => {
+                let currDate = (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate());
+                let sevenDay = (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + (date.getDate() - 7));
+                rangeFrom = sevenDay;
+                rangeTo = currDate;
+                return convertFormatData(user.registrationDate) <= currDate && convertFormatData(user.registrationDate) >= sevenDay;
+            });
+            getUsers(newUsers, header);
+            // showRange(sevenDay, currDate);
+            showRange(rangeFrom, rangeTo);
+        }
+        //         return new Date(convertFormatData(user.registrationDate)) >= new Date('2020-03-02') && new Date(convertFormatData(user.registrationDate)) <= new Date('2020-03-27')
+        
+
+
+
+
+
+        function convertFormatData(date) {
+            const arrMonth = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+            let newDate = date.replace(/[,]/g, "").split(' ');
+            arrMonth.forEach((month, index) => {
+                if (newDate[0] == month) {
+                    newDate[0] = index + 1;
+                    return newDate;
+                }
+            })
+            date = newDate[2] + '-' + newDate[0] + '-' + newDate[1];
+            return date
+        }
+
+
+        function showRange(rangeFrom='', rangeTo=''){
+            document.getElementsByClassName('pannel__range')[0].classList.remove('hide');
+            let text = document.querySelector('.pannel__range--text');
+            if ( !rangeTo ){
+                text.innerText = rangeFrom;
+            }else{
+//вывести коректно формат
+                text.innerText = 'Fistashka'
+            }
+            
+            // showRange.addEventListener('click', () => {
+            //     console.log('hi')
+            // });
+            //         //  let date = new Date();
+//         //  let month = new Date().toLocaleString('ru', { month: 'long' });
+//         //  console.log(month[0].toUpperCase()+month.slice(1) + ' ' + date.getDay() + ', ' + date.getFullYear() )
+        }
+
+
+
+    } catch (err) {
+        alert('Ошибка ' + err.name);
+    }
+})
+
+
+
+//         function checkDateEntrance(date, period) {
+
+
+            // let rusDate = "Декабрь 10, 2020"; // => 2020-03-10
+            // function convertFormatData(date){
+            //     const arrMonth=['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь', 'Октябрь','Ноябрь','Декабрь'];
+            //     // let newRuDate = rusDate.toLowerCase().replace(/[,]/g, "").split(' ');
+            //     let newDate = date.replace(/[,]/g, "").split(' ');
+            //     arrMonth.forEach( (month, index) => {
+            //         if( newDate[0] == month ){
+            //             newDate[0] = index + 1 ;
+            //             return newDate;
+            //         }
+            //     })
+            //     newDate = newDate[2] + '-' + newDate[0]  + '-'  + newDate[1];
+            //     return newDate
+            // } 
+            // console.log(  )
+
+
+
+
+//             // let options = {
+//             //     year: "numeric",
+//             //     month: "long",
+//             //     day: "numeric"
+//             //   }
+//             // console.log(new Date().toLocaleDateString("ru-RU", options));
+
+
+//             //перевести date  в формат 2017-04-01
+//             let periods = period.split(',');
+//             //console.log(periods) ["2017-04-01", "2020-04-27"]
+//             // console.log(new Date(periods[0]))
+//             // console.log(new Date( Date.parse("2017-04-01") ))
+
+
+
+//             if (new Date( convertFormatData(rusDate) ) >= new Date(periods[0]) && date <= new Date(periods[1]))
+//               console.log('УРА!');
+//             else
+//               console.log('ЖАЛЬ :(');
+//           }
+
+//           checkDateEntrance(new Date(), '2017-04-01,2020-04-27');
+
+
+
+//     }
+//     catch (err) {
+//         alert('Ошибка ' + err.name);
+//     }
+
+
+//  })
+
+
+//  function getRange( lastDay ){
+
+
+//     // return new Table sort
+//  }
